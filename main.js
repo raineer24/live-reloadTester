57c7b42515d884da64081d561d7bd8a6c29a6030
Raineer <delaritaraineer81@gmail.com>
2018-02-28 18:38:38 +0800

    async jquery

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 window.onload = function () {
   var http = new XMLHttpRequest();
+
+  http.onreadystatechange = function () {
+    if(http.readyState == 4 && http.status == 200){
+        //console.log(http.response); object
+       // console.log(JSON.parse(http.response)); json
+    }
+  };
   http.open("GET", "data/grocer.json", true);
   http.send();
-  console.log(http);
+
+  $.get("data/grocer.json", function (data) {
+    console.log(data);
+  });
+  console.log("test");
+
+
 };
 
 
